b9c7a88283f30510ffba8636981abea58837d548
shuai <651892631@qq.com>
2017-05-09 11:24:19 +0800

    lol

---
+++ js.js
@@ -0,0 +1,23 @@
+/**
+ * Created by DELL on 2017/3/23.
+ */
+window.onload=function(){
+    var img=document.getElementById("img");
+    var lb=document.getElementById("lb");
+    var btn=lb.getElementsByTagName("li");
+    btn[0].onmouseover=function(){
+        img.src="images/114516euooohhw8z88hghm.jpg";
+    };
+    btn[1].onmouseover=function(){
+        img.src="images/160743grxifrg8y0i4xf8l.png";
+    };
+    btn[2].onmouseover=function(){
+        img.src="images/lunbo.jpg";
+    };
+    btn[3].onmouseover=function(){
+        img.src="images/155142zzl4wcxscwv8cbcv.png";
+    };
+    btn[4].onmouseover=function(){
+        img.src="images/102858hubbxbb3b2d22wix.png";
+    }
+}
